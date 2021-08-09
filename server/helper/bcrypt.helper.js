0cc7c947191d25ec8992f53f4a91d4f95b233b2b
mahdi1297 <57314641+mahdi1297@users.noreply.github.com>
2021-08-09 12:16:19 +0430

    convert to 3s6 modules

--- server/helper/bcrypt.helper.js
+++ server/helper/bcrypt.helper.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+import bcrypt from "bcrypt"
 const saltRounds = 10;
 
 const hashPassword = (plainPassword) => {
@@ -18,7 +18,7 @@ const comparePassword = (plainPassword, passwordFromDb) => {
   });
 };
 
-module.exports = {
+export {
   hashPassword,
   comparePassword,
 };
