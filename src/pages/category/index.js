4782e4908c37055ba8a70483cef551bf0fc03fbe
m.alipour <mahdi9193514418@gmail.com>
2021-08-15 16:56:47 +0430

    start category page

---
+++ src/pages/category/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CategoryPage from "../../components/category";
+
+const Category = () => {
+  return (
+    <CategoryPage />
+  );
+};
+
+export default Category;
---
+++ src/pages/category/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+    width: 100%;
+    margin: auto;
+    margin-top: 100px;
+    @media (min-width: 700px){
+        width: 95%;
+    }
+    max-width: 1700px!important;
+`
