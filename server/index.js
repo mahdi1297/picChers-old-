6b045521764d50adcecf43bd12243bf4687abc61
m.alipour <mahdi9193514418@gmail.com>
2021-08-16 13:22:31 +0430

    deploy

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const sts = require("strict-transport-security");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 require("dotenv").config();
 
